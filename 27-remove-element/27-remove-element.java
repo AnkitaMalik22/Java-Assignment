8bb7f83feb638322d5b4a56d32697e6fa0d0f52b
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2022-07-22 20:27:41 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (60.68%) - LeetHub

---
+++ 27-remove-element/27-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count=0,i=0;
+        while(i<nums.length){
+            if(nums[i]!=val){
+                nums[count++]=nums[i];
+            }
+            i++;
+        }
+        return count;
+    }
+}
