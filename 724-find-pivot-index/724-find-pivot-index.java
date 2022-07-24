a237a242784c90cb732b859c50aa57c714e35c35
Ankita Malik <80139836+AnkitaMalik22@users.noreply.github.com>
2022-07-24 22:08:58 +0530

    Time: 2 ms (78.80%), Space: 52 MB (70.12%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+       for(int i=0;i<nums.length;i++){
+           sum+=nums[i];
+       }
+        int sum2=0;
+        for(int i=0;i<nums.length;i++){
+           sum-=nums[i];
+       if(sum==sum2){
+           return i;
+       }
+            sum2 +=nums[i];
+        }
+        
+        return -1;
+        
+    }
+}
